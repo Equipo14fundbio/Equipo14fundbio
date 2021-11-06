2659abdfea1dbdac00ce538762bd598769e6648a
Equipo14fundbio <89602952+Equipo14fundbio@users.noreply.github.com>
2021-11-05 23:55:42 -0500

    Create Activity_main(conf.partes.pag.in).java

---
+++ Software/Activity_main(conf.partes.pag.in).java
@@ -0,0 +1,29 @@
+<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:background="#9F0A6A6A"
+    tools:context=".MainActivity">
+
+    <Button
+        android:id="@+id/NuevoUsuario"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_centerInParent="true"
+        android:text="Nuevo Usuario" />
+
+    <ImageView
+        android:id="@+id/imageView"
+        android:layout_width="316dp"
+        android:layout_height="329dp"
+        android:layout_alignParentStart="true"
+        android:layout_alignParentEnd="true"
+        android:layout_alignParentBottom="true"
+        android:layout_marginStart="47dp"
+        android:layout_marginEnd="47dp"
+        android:layout_marginBottom="395dp"
+        app:srcCompat="@mipmap/logo" />
+
+</RelativeLayout>
