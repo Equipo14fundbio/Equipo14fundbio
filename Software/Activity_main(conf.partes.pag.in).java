dfce3f7726f9161caa7cece4a0c0e99766ccc151
Equipo14fundbio <89602952+Equipo14fundbio@users.noreply.github.com>
2021-11-15 18:01:38 -0500

    Update Activity_main(conf.partes.pag.in).java

--- Software/Activity_main(conf.partes.pag.in).java
+++ Software/Activity_main(conf.partes.pag.in).java
@@ -1,29 +1,26 @@
 <?xml version="1.0" encoding="utf-8"?>
-<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
     xmlns:app="http://schemas.android.com/apk/res-auto"
     xmlns:tools="http://schemas.android.com/tools"
     android:layout_width="match_parent"
     android:layout_height="match_parent"
-    android:background="#9F0A6A6A"
-    tools:context=".MainActivity">
+    tools:context=".MainActivity"
+    android:orientation="vertical"
+    android:background="#FFFFFF">
 
-    <Button
-        android:id="@+id/NuevoUsuario"
-        android:layout_width="wrap_content"
+
+  <ImageView
+      android:layout_marginTop="35dp"
+      android:layout_gravity="center"
+      android:layout_width="300dp"
+      android:layout_height="300dp"
+      android:src="@mipmap/logo"/>
+
+    <androidx.recyclerview.widget.RecyclerView
+        android:id="@+id/listaPacientes"
+        android:layout_width="match_parent"
         android:layout_height="wrap_content"
-        android:layout_centerInParent="true"
-        android:text="Nuevo Usuario" />
+        android:scrollbarSize="30dp" />
 
-    <ImageView
-        android:id="@+id/imageView"
-        android:layout_width="316dp"
-        android:layout_height="329dp"
-        android:layout_alignParentStart="true"
-        android:layout_alignParentEnd="true"
-        android:layout_alignParentBottom="true"
-        android:layout_marginStart="47dp"
-        android:layout_marginEnd="47dp"
-        android:layout_marginBottom="395dp"
-        app:srcCompat="@mipmap/logo" />
 
-</RelativeLayout>
+</LinearLayout>
